66b34ec31a4492c5a9dc6ba3a9e8d83ef6b6d0db
Ferdian <ferdian.artwork@gmail.com>
2021-04-08 06:08:39 +0000

    last update

--- bad/index.js
+++ bad/index.js
@@ -19,24 +19,15 @@ function chunkArray(myArray, chunk_size){
     return tempArray;
 }
 
-
+// load gif file to buffer
 const gif_buffer = fs.readFileSync("out.gif");
-
-let gif_info = gify.getInfo(gif_buffer);
-const gif_pal = new Map();
-gif_info.globalPaletteColorsRGB.map((x, index) => {
-	gif_pal.set(`${x.r}:${x.g}:${x.b}`, index);
-});
-
-gif_info = null;
 
 var gifData = new omg.GifReader(gif_buffer);
 
-const stream = fs.createWriteStream("data_raw.dat", {flags: "a"});
-
-
 const num_pixels = gifData.width * gifData.height;
 let current, buffer, newBlit, ca
+
+let finalBlit = []
 
 for (let i=0; i<gifData.numFrames(); i++) {
 	current = gifData.frameInfo(i);
@@ -44,7 +35,7 @@ for (let i=0; i<gifData.numFrames(); i++) {
 	buffer = new Uint8Array(num_pixels * 4);
 	gifData.decodeAndBlitFrameRGBA(i, buffer)
 
-	newBlit = new Uint8Array(num_pixels);
+	newBlit = [];
 
 	var index = 0;
 	var indexed_num = 0;
@@ -54,15 +45,89 @@ for (let i=0; i<gifData.numFrames(); i++) {
     for (index = 0; index < arrayLength; index += chunk_size) {
         myChunk = buffer.slice(index, index+chunk_size);
         // Do something if you want with the group
-        if (!gif_pal.has(`${myChunk[0]}:${myChunk[1]}:${myChunk[2]}`)) throw `pal not found ${myChunk[0]}:${myChunk[1]}:${myChunk[2]}`;
-        let indexed = gif_pal.get(`${myChunk[0]}:${myChunk[1]}:${myChunk[2]}`)
-		newBlit[indexed_num] = indexed;
+        if (myChunk[3] != 255 && i == 0) throw new Error(`${myChunk[0]},${myChunk[1]},${myChunk[2]},${myChunk[3]}`)
+		newBlit.push(`${myChunk[0]},${myChunk[1]},${myChunk[2]},${myChunk[3]}`);
 		indexed_num++;
     }
 
+    finalBlit.push(newBlit);
+
 	console.log(i, gifData.numFrames());
 
-	stream.write(newBlit);
 }
 
-stream.end();
+// remove any transparent
+for (let i=0; i<finalBlit.length; i++) {
+    let temp = [];
+
+    for(let i2=0; i2<finalBlit[i].length; i2++) {
+        let rgb = finalBlit[i][i2].split(",");
+
+        // console.log(rgb)
+
+        if (rgb[3] == 0) {
+            temp.push(finalBlit[i-1][i2]);
+        } else {
+            temp.push(finalBlit[i][i2]);
+        }
+
+    }
+
+    finalBlit[i] = temp;
+}
+
+// get palette with map
+const gif_pal = new Map();
+const gif_pal_array = [];
+
+finalBlit.forEach(blit => {
+    for(let i=0; i<blit.length; i++) {
+        gif_pal.set(blit[i], "random value");
+    }
+})
+
+// convert map to array
+for (const [key, value] of gif_pal.entries()) {
+    gif_pal_array.push(key);
+}
+
+// console.log(gif_pal_array);
+// return;
+
+// format finalBlit
+
+finalBlit = finalBlit.map(blit => {
+    let temp = []
+    for(let i=0; i<blit.length; i++) {
+        temp.push(gif_pal_array.indexOf(blit[i]));
+    }
+
+    return temp;
+})
+
+
+
+// draw
+
+finalBlit.forEach((blit, index) => {
+    let final_out = '';
+    console.log("frame", index);
+
+    for (i = 0; i < blit.length; i += gifData.width) {
+        let chunk = blit.slice(i, i+gifData.width);
+
+        chunk.forEach(x => {
+            // final_out += x;
+            if (x == 0) final_out += "▓"
+            else if (x == 1) final_out += "▒"
+            else if (x == 2) final_out += "░"
+            else if (x == 3) final_out += "⠀"
+            else if (x == 4) final_out += "⠀"
+        })
+
+        final_out += "\n"
+    }
+
+
+    console.log(final_out)
+})
--- bad/video/run.sh
+++ bad/video/run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-palette="/home/melody/Desktop/github/random-crap/bad/video/palette.png"
+palette="/home/melody/Desktop/random-crap/bad/video/palette.png"
 video="video.mp4"
 output="out.gif"
 
-filters="fps=30,scale=256x192"
-skip="-t 1 -ss 00:10"
+filters="fps=3,scale=16x9"
+skip="-t 1 -ss 00:15"
 
-ffmpeg  -i $video -vf "$filters,palettegen" -y $palette
+ffmpeg  -i $video -vf "$filters,palettegen=max_colors=4:reserve_transparent=" -y $palette
 ffmpeg  -i $video -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse=dither=none" -y $output
